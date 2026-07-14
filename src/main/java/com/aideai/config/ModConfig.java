9fe74344b2a3f8945315bb427b616574b1fcf677
AideAI <aideai@dev.com>
2026-07-14 09:08:49 +0000

    Fix: register ModConfig with ModLoadingContext so set() actually saves to disk

--- src/main/java/com/aideai/config/ModConfig.java
+++ src/main/java/com/aideai/config/ModConfig.java
@@ -1,7 +1,8 @@
 package com.aideai.config;
 
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.ModConfigSpec;
-import org.apache.commons.lang3.tuple.Pair;
 
 public class ModConfig {
     public static class Client {
@@ -10,9 +11,6 @@ public class ModConfig {
         public final ModConfigSpec.ConfigValue<String> model;
         public final ModConfigSpec.ConfigValue<Integer> autoInterval;
         public final ModConfigSpec.ConfigValue<Boolean> autoChatEnabled;
-        public final ModConfigSpec.ConfigValue<Boolean> prankEnabled;
-        public final ModConfigSpec.ConfigValue<Integer> prankChance;
-        public final ModConfigSpec.ConfigValue<Integer> openKey;
         
         public Client(ModConfigSpec.Builder builder) {
             builder.push("AI Settings");
@@ -25,15 +23,6 @@ public class ModConfig {
             autoChatEnabled = builder.comment("Enable auto chat").define("autoChatEnabled", false);
             autoInterval = builder.comment("Auto chat interval (seconds)").defineInRange("autoInterval", 300, 0, 3600);
             builder.pop();
-            
-            builder.push("Prank Settings");
-            prankEnabled = builder.comment("Prank mode").define("prankEnabled", true);
-            prankChance = builder.comment("Prank chance (percent)").defineInRange("prankChance", 15, 0, 100);
-            builder.pop();
-            
-            builder.push("Controls");
-            openKey = builder.comment("Key to open GUI (GLFW key code)").defineInRange("openKey", 72, 0, 400);
-            builder.pop();
         }
     }
     
@@ -41,10 +30,12 @@ public class ModConfig {
     public static final Client CLIENT;
     
     static {
-        final Pair<Client, ModConfigSpec> specPair = new ModConfigSpec.Builder().configure(Client::new);
-        CLIENT_SPEC = specPair.getRight();
-        CLIENT = specPair.getLeft();
+        final var pair = new ModConfigSpec.Builder().configure(Client::new);
+        CLIENT_SPEC = pair.getRight();
+        CLIENT = pair.getLeft();
     }
     
-    public static void init() {}
+    public static void init() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_SPEC, "aideai-client.toml");
+    }
 }
